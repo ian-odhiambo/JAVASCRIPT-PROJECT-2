6a45e15bc2fc1f0d463b2448b73fa7dac955c52a
ian <odhiamboian096>
2026-03-11 08:14:05 +0300

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//string manipulation functions
-//1. REVERSE A STRING
+//STRING MANIPULATION FUNCTIONS
+//1. a function that reverses a given string.
 function reverseString(str) {
     let reversed = '';
     for (let i = str.length - 1; i >= 0; i--) {
